1b6b504fa79724afc0bcf5045b45bb466c0b9336
kartik18ai-coder <kartik9607k@gmail.com>
2026-03-11 21:00:52 +0530

    OOPS banner using string arrays and loops

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,28 +1,24 @@
-public class OOPSBannerApp {
+ublic class OOPSBannerApp {
 
     // Main method to run the banner display
     public static void main(String[] args) {
-       
-        
-       
-        System.out.println(String.join("", " * ", "  * ", " ** ", "  ** "));
-        
-      
-        System.out.println(String.join("", "* *", " * *", " * *", " ** "));
-        
-      
-        System.out.println(String.join("", "* *", " * *", " * *", "  ** "));
-        
-       
-        System.out.println(String.join("", "* *", " * *", " ** ", "    ** "));
-        
-     
-        System.out.println(String.join("", "* *", " * *", " * ", "       * "));
-        
-      
-        System.out.println(String.join("", "* *", " * *", " * ", "      *"));
-        
-        
-        System.out.println(String.join("", " * ", "  * ", " * ", "   *** "));
+
+        // 1. Define a String array with a size equal to the 7 lines of the banner 
+        String[] lines = new String[7];
+
+        // 2. Populate each index of the array using String.join()
+        // Maintaining 7-line format with 9-character width per letter 
+        lines[0] = String.join("", "  * ", "   * ", "  ** ", "   ** ");
+        lines[1] = String.join("", " * * ", " * * ", " * * ", " ** ");
+        lines[2] = String.join("", " * * ", " * * ", " * * ", "  ** ");
+        lines[3] = String.join("", " * * ", " * * ", " ** ", "    ** ");
+        lines[4] = String.join("", " * * ", " * * ", " * ", "       * ");
+        lines[5] = String.join("", " * * ", " * * ", " * ", "      * ");
+        lines[6] = String.join("", "  * ", "   * ", "  * ", "   *** ");
+
+        // 3. Use an enhanced for-each loop to print each line [cite: 181, 186, 213]
+        for (String line : lines) {
+            System.out.println(line); 
+        }
     }
 }
